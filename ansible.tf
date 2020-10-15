4ef62b636cc3922e75faaf0631c6eed1da33a7ca
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-15 07:46:40 -0600

    ADD: Resources to install ansible in Terraform Cloud and execute the ansible-playbook.

---
+++ ansible.tf
@@ -0,0 +1,21 @@
+# Install ansible in Terraform Cloud
+resource "null_resource" "install_ansible" {
+  provisioner "local-exec" {
+    command = "python -m pip install --user ansible==2.9.14 openshift==0.11.2"
+  }
+  triggers = {
+    build_number = timestamp()
+  }
+  depends_on = [google_container_cluster.gke]
+}
+
+# Run the Ansible playbook locally against the GKE Cluster
+resource "null_resource" "run_ansible_playbook" {
+  provisioner "local-exec" {
+    command = "export PATH=$PATH:$HOME/.local/bin ; export K8S_AUTH_KUBECONFIG=${abspath(local_file.kubeconfig.filename)} ; ansible-playbook ansible/playbooks/install-kong.yml"
+  }
+  triggers = {
+    build_number = timestamp()
+  }
+  depends_on = [null_resource.install_ansible,local_file.kubeconfig]
+} 
