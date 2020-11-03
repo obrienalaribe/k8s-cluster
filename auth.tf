edb1b2656f154e3f50cd7d041bddd99c49fe7fd1
Alexander Urbina <w.alexanderuc@gmail.com>
2020-11-03 17:14:53 -0600

    REFACTOR: GKE Auth needs to have the node pool dependency instead of kubeconfig.

--- auth.tf
+++ auth.tf
@@ -2,14 +2,13 @@
 # Submodule is used to generate a kubeconfig file
 module "gke_auth" {
   source           = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-
   project_id       = var.project
   cluster_name     = var.gke_cluster_name
   location         = var.zone
+  depends_on = [google_container_node_pool.default_node_pool]
 }
 
 resource "local_file" "kubeconfig" {
-  depends_on = [google_container_node_pool.default_node_pool]
   content  = module.gke_auth.kubeconfig_raw
   filename = ".kubeconfig"
 }
