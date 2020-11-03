8cf355e25ea005744b93f07fb51cc393b6d41e36
Alexander Urbina <w.alexanderuc@gmail.com>
2020-11-03 17:13:51 -0600

    ADD: Terraform outputs.

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "gke_cluster_name" {
+  value = var.gke_cluster_name
+}
+
+output "consul_k8s_namespace" {
+  value = var.consul_k8s_namespace
+}
+
+output "consul_release_name" {
+  value = var.consul_release_name
+}
