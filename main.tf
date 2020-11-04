1df97ce77bd7c728dcfbcff0a7c1e107613a9058
Alexander Urbina <w.alexanderuc@gmail.com>
2020-11-04 17:59:58 -0600

    FIX: Use module.gke_auth.* data instead of local kubeconfig in kubernetes and helm providers.

--- main.tf
+++ main.tf
@@ -7,12 +7,18 @@ provider "google" {
 
 provider "kubernetes" {
   version = "~> 1.11"
-  config_path = abspath(local_file.kubeconfig.filename)
+  load_config_file = false
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  host                   = module.gke_auth.host
+  token                  = module.gke_auth.token
 }
 
 provider "helm" {
   version = "~> 1.3"
   kubernetes {
-    config_path = abspath(local_file.kubeconfig.filename)
+      load_config_file = false
+      cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+      host                   = module.gke_auth.host
+      token                  = module.gke_auth.token
   }
 }
