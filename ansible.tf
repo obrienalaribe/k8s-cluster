3caf4bb7ef7a2e165505b64bb3f547e2bab9ffef
Alexander Urbina <w.alexanderuc@gmail.com>
2021-04-27 07:51:31 -0600

    REFACTOR: Ansible Playbooks and add variables to split installation and configuration of Rook and Kong.

--- ansible.tf
+++ ansible.tf
@@ -1,8 +1,9 @@
 # Install ansible and openshift package in Terraform Cloud
 resource "null_resource" "install_ansible" {
-  count = var.enable_kong ? 1 : 0
+  count = var.enable_kong || var.enable_rook ? 1 : 0
   provisioner "local-exec" {
-    command = "python3 -m pip install -U pip && python3 -m pip install --user ansible==2.9.14 openshift==0.11.2"
+    #command = "python3 -m pip install -U pip && python3 -m pip install --user ansible==2.9.14 openshift==0.11.2"
+    command = "echo install_ansible"
   }
   triggers = {
     build_number = timestamp()
@@ -11,7 +12,7 @@ resource "null_resource" "install_ansible" {
 }
 
 # Run the Ansible playbook locally against the GKE Cluster
-resource "null_resource" "run_ansible_playbook" {
+resource "null_resource" "run_kong_ansible_playbook" {
   count = var.enable_kong ? 1 : 0
   provisioner "local-exec" {
     command = "export PATH=$PATH:$HOME/.local/bin ; export K8S_AUTH_KUBECONFIG=${abspath(local_file.kubeconfig.filename)} ; ansible-playbook ansible/playbooks/install-kong.yml"
@@ -20,4 +21,15 @@ resource "null_resource" "run_ansible_playbook" {
     build_number = timestamp()
   }
   depends_on = [null_resource.install_ansible,local_file.kubeconfig]
-} 
+}
+
+resource "null_resource" "run_rook_ansible_playbook" {
+  count = var.enable_rook ? 1 : 0
+  provisioner "local-exec" {
+    command = "export PATH=$PATH:$HOME/.local/bin ; export K8S_AUTH_KUBECONFIG=${abspath(local_file.kubeconfig.filename)} ; ansible-playbook ansible/playbooks/install-rook.yml"
+  }
+  triggers = {
+    build_number = timestamp()
+  }
+  depends_on = [null_resource.install_ansible,local_file.kubeconfig]
+}
