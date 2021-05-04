5e46efbcd19ad2668a270810de710f8a31d43f3c
adrixnac <adrianutm@gmail.com>
2021-05-04 18:47:44 -0500

    REFACTOR: Format terraform documents

--- resources.tf
+++ resources.tf
@@ -1,22 +1,22 @@
 # Create the GKE cluster removing the default node pool created by GKE upon cluster creation
 resource "google_container_cluster" "gke" {
-  name     = var.gke_cluster_name
-  location = var.region
-  node_locations = var.zones
+  name                     = var.gke_cluster_name
+  location                 = var.region
+  node_locations           = var.zones
   remove_default_node_pool = true
   initial_node_count       = 1
   ip_allocation_policy {} # Define block for VPC-Native Cluster
   maintenance_policy {
     recurring_window {
-    start_time = "2021-03-23T00:00:00Z"
-    end_time = "2021-03-23T06:00:00Z"
-    recurrence = "FREQ=WEEKLY;BYDAY=SA,SU"
+      start_time = "2021-03-23T00:00:00Z"
+      end_time   = "2021-03-23T06:00:00Z"
+      recurrence = "FREQ=WEEKLY;BYDAY=SA,SU"
     }
   }
   private_cluster_config {
-    enable_private_nodes = true
+    enable_private_nodes    = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block = var.gke_cluster_master_cidr_block
+    master_ipv4_cidr_block  = var.gke_cluster_master_cidr_block
   }
   enable_shielded_nodes = true
   vertical_pod_autoscaling {
@@ -26,22 +26,22 @@ resource "google_container_cluster" "gke" {
     enabled = true
     resource_limits {
       resource_type = "cpu"
-      maximum = var.gke_cluster_autoscaling_max_cpu_cores
-      minimum = var.gke_cluster_autoscaling_min_cpu_cores
+      maximum       = var.gke_cluster_autoscaling_max_cpu_cores
+      minimum       = var.gke_cluster_autoscaling_min_cpu_cores
     }
     resource_limits {
       resource_type = "memory"
-      maximum = var.gke_cluster_autoscaling_max_memory_gb
-      minimum = var.gke_cluster_autoscaling_min_memory_gb
+      maximum       = var.gke_cluster_autoscaling_max_memory_gb
+      minimum       = var.gke_cluster_autoscaling_min_memory_gb
     }
   }
 }
 
 # Create the new managed default node pool with autoscaling
 resource "google_container_node_pool" "default_node_pool" {
-  name       = var.gke_node_pool_name
+  name     = var.gke_node_pool_name
   location = var.region
-  cluster    = google_container_cluster.gke.name
+  cluster  = google_container_cluster.gke.name
   # Define initial number of nodes for the pool with a lifecycle block to ignore subsequent changes to this field
   initial_node_count = var.gke_initial_node_count
 
@@ -52,8 +52,8 @@ resource "google_container_node_pool" "default_node_pool" {
   }
 
   autoscaling {
-      min_node_count = var.gke_min_node_count
-      max_node_count = var.gke_max_node_count
+    min_node_count = var.gke_min_node_count
+    max_node_count = var.gke_max_node_count
   }
 
   node_config {
--- vars.tf
+++ vars.tf
@@ -1,52 +1,52 @@
 # Google Vars
 variable "project" {
-  type = string
+  type        = string
   description = "GCP Project"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "GCP Region"
 }
 
 variable "zones" {
-  type = list(string)
+  type        = list(string)
   description = "GCP Zones"
 }
 
 # GKE Vars
 variable "gke_cluster_name" {
-  type = string
+  type        = string
   description = "GKE cluster name"
 }
 
 variable "gke_node_pool_name" {
-  type = string
+  type        = string
   description = "GKE manage node pool name"
 }
 
 variable "gke_node_pool_machine_type" {
-  type = string
+  type        = string
   description = "Instance type used upon node pool creation"
 }
 
 variable "gke_initial_node_count" {
-  type = number
+  type        = number
   description = "The initial number of nodes for the pool."
 }
 
 variable "gke_min_node_count" {
-  type = number
+  type        = number
   description = "Minimum number of nodes in the NodePool"
 }
 
 variable "gke_max_node_count" {
-  type = number
+  type        = number
   description = "Maximum number of nodes in the NodePool"
 }
 
 variable "gke_cluster_master_cidr_block" {
-  type = string
+  type        = string
   description = "The IP range in CIDR notation to use for the hosted master network"
 }
 
@@ -76,21 +76,21 @@ variable "gke_cluster_autoscaling_min_memory_gb" {
 
 
 variable "enable_kong" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Whether or not to deploy Kong"
 }
 
 variable "enable_rook" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Whether or not to deploy Rook"
 }
 
 # Hashicorp Vars
 variable "enable_vault_consul" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Whether or not to deploy Vault and Consul"
 }
 
