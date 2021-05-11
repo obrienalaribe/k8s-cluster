184d3bab22f75a03e3f34a7ed28951a9fe774a04
Alexander Urbina <42731074+urbinaalex17@users.noreply.github.com>
2021-05-11 15:09:57 -0500

    ADD: Load Balancer Source Ranges in Helm Release Chart to only allow Cloudflare to access NGNX Ingress Controller public IP Address. (#3)

--- helm.tf
+++ helm.tf
@@ -47,4 +47,26 @@ resource "helm_release" "nginx_ingress" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   namespace  = kubernetes_namespace.nginx.0.id
+  values = [
+    <<EOF
+    controller:
+      service:
+        loadBalancerSourceRanges:
+          - 173.245.48.0/20
+          - 103.21.244.0/22
+          - 103.22.200.0/22
+          - 103.31.4.0/22
+          - 141.101.64.0/18
+          - 108.162.192.0/18
+          - 190.93.240.0/20
+          - 188.114.96.0/20
+          - 197.234.240.0/22
+          - 198.41.128.0/17
+          - 162.158.0.0/15
+          - 172.64.0.0/13
+          - 131.0.72.0/22
+          - 104.16.0.0/13
+          - 104.24.0.0/14
+    EOF
+  ]
 }
